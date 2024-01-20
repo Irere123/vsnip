08a7d7fdf7371d8e9460511ca35da7dafa7967a0
Irere123 <irere2020@outlook.com>
2024-01-20 12:12:30 +0200

    changes

--- extension/webviews/screens/EditProfile.svelte
+++ extension/webviews/screens/EditProfile.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import type { ProfileFormData, State } from "../shared/types";
   import Backbar from "../ui/Backbar.svelte";
+  import { query } from "../shared/query";
 
   export let data: ProfileFormData;
   export let onUpdate: () => void;
   export let onNewState: (s: State) => void;
+
+  onMount(async () => {
+    try {
+      const payload = await query("/conversations/100");
+      console.log(payload);
+    } catch {}
+  });
 </script>
 
 <Backbar
