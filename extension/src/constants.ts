d2ff34a9eed248da752371187169084e8da1d491
Irere123 <irere2020@outlook.com>
2024-01-25 09:25:46 +0200

    optimizations for publish

--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -1,6 +1,6 @@
 export const _prod_ = process.env.NODE_ENV === "production";
 export const apiBaseUrl = _prod_
-  ? "https://api.vsinder.com"
+  ? "https://vsnip.onrender.com"
   : "http://localhost:4000";
-export const accessTokenKey = "@vsinder/token" + (_prod_ ? "" : "dev");
-export const refreshTokenKey = "@vsinder/refresh-token" + (_prod_ ? "" : "dev");
+export const accessTokenKey = "@vsnip/token" + (_prod_ ? "" : "dev");
+export const refreshTokenKey = "@vsnip/refresh-token" + (_prod_ ? "" : "dev");
